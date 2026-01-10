6e2cf8162bfe39f37854e26ee63785b16e8d25b3
RR77ui <raveldo567@gmail.com>
2026-01-10 10:50:56 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -15,3 +15,40 @@ console.log(o.a)
 const o3 = Object.assign({},o)
 
 console.log(o3)
+
+//Cada objeto que se cree se guarda en una heap(Memoria de JS) pero cuando se copia este objeto y se cambia alguna variable o propiedad de los objetos esta se cambiaria para ambos objetos
+
+const num = 5
+
+const implicito = num + ""
+
+const explicita = String(num)
+
+console.log(typeof implicito, typeof explicita)
+
+
+arr = [
+    "miguel", "rudas"  
+]
+
+arr.push("valeria")
+
+console.log(arr)
+
+const str = "hello world"
+
+console.log(str.toUpperCase())
+console.log(arr.__proto__)
+// Herencia prototipal: Cada variable hereda los metodos de cada tipo de dato, en este caso el arreglo arr puede utilizar .push, .delete, etc.
+
+function ft(){
+    console.log("i am hoisted")
+}
+
+const nh = function ft1(){
+    console.log()
+} 
+
+const text = "ABCDEFGHIJK"
+
+console.log(text.split(""))
